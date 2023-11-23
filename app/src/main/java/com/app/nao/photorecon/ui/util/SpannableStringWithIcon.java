b184d880cd48fe92147ade86bc77074abce01e4a
naoya0930 <utmost.gt.y.rgx@gmail.com>
2023-11-23 11:57:18 +0900

    add:アイコンがない場合の処理の追加．一旦スルーしてエラー処理は未実装

--- app/src/main/java/com/app/nao/photorecon/ui/util/SpannableStringWithIcon.java
+++ app/src/main/java/com/app/nao/photorecon/ui/util/SpannableStringWithIcon.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.text.Spannable;
 import android.text.SpannableString;
+import android.text.TextPaint;
 import android.text.style.ImageSpan;
+import android.widget.TextView;
 
 import androidx.annotation.DrawableRes;
 import androidx.core.content.ContextCompat;
@@ -14,10 +16,20 @@ public class SpannableStringWithIcon {
         SpannableString spannableString = new SpannableString("  " + text); // スペースを追加してアイコンとテキストを区切る
         // アイコンを挿入
         Drawable icon = ContextCompat.getDrawable(context, drawableID);
-        icon.setBounds(0, 0, icon.getIntrinsicWidth(), icon.getIntrinsicHeight());
-        ImageSpan imageSpan = new ImageSpan(icon, ImageSpan.ALIGN_BASELINE);
-        spannableString.setSpan(imageSpan, 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        if (icon != null) {
+            // テキストサイズに合わせてアイコンサイズを調整
+            // TODO: テキストサイズは元のウィンドウに沿ってして使用したい．
+            // int textSize = (int) (24); // テキストサイズの1.5倍
+            // icon.setBounds(0, 0, textSize, textSize);
+            icon.setBounds(0, 0, icon.getIntrinsicWidth(), icon.getIntrinsicHeight());
+            ImageSpan imageSpan = new ImageSpan(icon, ImageSpan.ALIGN_BASELINE);
+            spannableString.setSpan(imageSpan, 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
 
+            // ImageSpan を作成してアイコンをセット
+        }
+        icon.setBounds(0, 0, icon.getIntrinsicWidth(), icon.getIntrinsicHeight());
         return spannableString;
+//
+//        return spannableString;
     }
 }
